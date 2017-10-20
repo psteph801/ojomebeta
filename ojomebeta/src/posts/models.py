9facb9bbf3eb1f4d9d127faa7525ba277a8b0735
psteph801 <axisbookk@gmail.com>
2017-10-19 20:39:34 -0600

    Changing Fields in Models
    https://www.codingforentrepreneurs.com/projects/tweetme/changing-fields-models/?play=true

--- ojomebeta/src/posts/models.py
+++ ojomebeta/src/posts/models.py
@@ -4,10 +4,12 @@ from django.db import models
 
 
 class post(models.Model):
-	posttitle		 = models.TextField()
+
 	description 	= models.TextField()
 	description1 	= models.TextField()
 	description2 	= models.TextField()
+	timestamp		= models.DateTimeField(auto_now=True)
+	updated			= models.DateTimeField(auto_now_add=True)
 	def __str__(self):
 		return str(self.posttitle)
 
