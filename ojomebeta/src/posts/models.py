81eab3bded6953c98d5679010373495c5efb4c63
psteph801 <axisbookk@gmail.com>
2017-10-19 20:51:11 -0600

    Associating a User to A Tweet Using Foreign Keys
    https://www.codingforentrepreneurs.com/projects/tweetme/associating-user-tweet-using-foreign-keys/?play=true#/lecture

--- ojomebeta/src/posts/models.py
+++ ojomebeta/src/posts/models.py
@@ -1,16 +1,18 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
 
 
 class post(models.Model):
-
-	description 	= models.TextField()
-	description1 	= models.TextField()
-	description2 	= models.TextField()
+	user			= models.ForeignKey(settings.AUTH_USER_MODEL)
+	description 	= models.TextField(max_length=280)
+	description1 	= models.TextField(max_length=280)
+	description2 	= models.TextField(max_length=280)
 	timestamp		= models.DateTimeField(auto_now=True)
 	updated			= models.DateTimeField(auto_now_add=True)
+	
 	def __str__(self):
-		return str(self.posttitle)
+		return str(self.description)
 
 		
